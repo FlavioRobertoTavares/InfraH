0eabfe751329138636001667464b95ccb98e967a
Gust4voSSM <gugu9santiago@gmail.com>
2024-02-09 03:30:59 -0300

    Adicionado registradores de estado e definido o estado inicial através de um macro; Reset implementado

--- Projeto IH/Controle.v
+++ Projeto IH/Controle.v
@@ -41,9 +41,32 @@ module Controle (
         
         output reg unsignedn
 );
-
         `include "Estados.vh"
 
+        //Macro para zerar todos os regitradores. Pode ser substituido por uma Task futuramente
+        `define REG_RESET \
+        {PC_src, ALU_src_A, ALU_src_B, shsrc, shamt, ir_write_data, ir_write_reg, bank_write_data, bank_write_reg} = {2'b00}; \
+        lorD = 0; \
+        {mem_read, mem_write, reg_write, ir_write, PC_write, bank_write, load_ctrl, store_ctrl, EPC_write} = {1'b0}; \
+        unsignedn = 0
+
+        reg [5:0]state;
+        reg [3:0]counter;
+
+        initial begin
+                `REG_RESET;
+        end
+
+        always @(posedge clk) begin
+                if (reset == 1) begin
+                        `REG_RESET;
+                        state = START;
+                        counter = 0;
+                end
+        end
+
+
+        
 
 
 endmodule
