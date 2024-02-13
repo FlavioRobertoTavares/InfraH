b6ce8c26095fe2d0469ebbf98307e72f2eb4d186
Flávio Roberto <124847034+FlavioRobertoTavares@users.noreply.github.com>
2024-02-12 23:41:50 -0300

    Uptade Load e Store

--- Projeto IH/Load.v
+++ Projeto IH/Load.v
@@ -2,14 +2,24 @@ module Load (
     input wire [31:0] RegMemo, input wire [1:0] LoadControl, output reg [31:0] saida);
     
     always @(posedge LoadControl) begin
-        if (LoadControl == 1)
-            saida = {2'b000000000000000000000000, RegMemo[7:0]};
-        else if (LoadControl == 2)
-            saida = {2'b0000000000000000 , RegMemo[15:0]};
-        else if (LoadControl == 3)
-            saida = RegMemo;
-        else
-            saida = x;
+        if (LoadControl == 1) begin
+            saida = {2'b000000000000000000000000, RegMemo[7:0]}; 
+        end
+
+        else if (LoadControl == 2) begin
+            saida = {2'b0000000000000000 , RegMemo[15:0]}; 
+        end
+
+        else if (LoadControl == 3) begin
+            saida = RegMemo; 
+        end
+
+        else begin
+            saida = x; 
+        end
+        
     end
 
 endmodule
+
+
--- Projeto IH/Store.v
+++ Projeto IH/Store.v
@@ -2,14 +2,22 @@ module Store (
     input wire [31:0] RegMemo, RegB, input wire [1:0] StoreControl, output reg [31:0] saida);
     
     always @(posedge StoreControl) begin
-        if (StoreControl == 1)
-            saida = {RegMemo[31:8], RegB[7:0]};
-        else if (StoreControl == 2)
-            saida = {RegMemo[31:16], RegB[15:0]};
-        else if (StoreControl == 3)
-            saida = RegB;
-        else
-            saida = x;
+        if (StoreControl == 1) begin
+            saida = {RegMemo[31:8], RegB[7:0]}; 
+        end
+
+        else if (StoreControl == 2) begin
+            saida = {RegMemo[31:16], RegB[15:0]}; 
+        end
+
+        else if (StoreControl == 3) begin
+            saida = RegB; 
+        end
+
+        else begin
+            saida = x; 
+        end
+        
     end
 
 endmodule
