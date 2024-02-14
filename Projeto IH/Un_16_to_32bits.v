b200f696ed067678428ef75c8bd603fabdce78a0
Flávio Roberto <124847034+FlavioRobertoTavares@users.noreply.github.com>
2024-02-14 15:34:16 -0300

    correção no nome do modulo

--- Projeto IH/Un_16_to_32bits.v
+++ Projeto IH/Un_16_to_32bits.v
@@ -1,4 +1,4 @@
-module moduleName (
+module Un_16_to_32bit (
     input wire Un, input wire [15:0] immediate, output wire [31:0] immediate_resultado);
 
     assign immediate_resultado = ( (Un == 0) && (immediate[15] == 0) )? {16'b0, immediate} :
