b5133e64c9c17746641db20c036a376a595c620c
Gust4voSSM <gugu9santiago@gmail.com>
2024-02-15 18:50:36 -0300

    Corrição de erros

--- Projeto IH/PC_and_offset_concatenator.v
+++ Projeto IH/PC_and_offset_concatenator.v
@@ -1,7 +1,7 @@
 module PC_and_offset_concatenator (
     input wire [25:0] offset, input wire [31:0] PC, output wire [31:0] offset_resultado);
 
-    assign offset = {offset, 2'b00};
-    assign offset_resultado = {PC[31:28], offset};
+    wire [27:0] offset_extendido = {offset, 2'b00};
+    assign offset_resultado = {PC[31:28], offset_extendido};
 
 endmodule
