ea8ee56a47aebc5bc85d03c8d496bb2a7545c7e8
Gust4voSSM <gugu9santiago@gmail.com>
2024-02-16 20:24:16 -0300

    Load e Store agora são combinacionais (MUXes)

--- Projeto IH/Load.v
+++ Projeto IH/Load.v
@@ -1,25 +1,9 @@
 module Load (
-    input wire [31:0] RegMemo, input wire [1:0] LoadControl, output reg [31:0] saida);
+    input wire [31:0] RegMemo, input wire [1:0] LoadControl, output wire [31:0] saida);
     
-    always @(posedge LoadControl) begin
-        if (LoadControl == 1) begin
-            saida = {24'b0, RegMemo[7:0]}; 
-        end
-
-        else if (LoadControl == 2) begin
-            saida = {16'b0 , RegMemo[15:0]}; 
-        end
-
-        else if (LoadControl == 3) begin
-            saida = RegMemo; 
-        end
-
-        else begin
-            saida = 32'hxxxxxxxx;
-        end
-        
-    end
+    assign saida = (LoadControl == 1)? {24'b0, RegMemo[7:0]} :
+                   (LoadControl == 2)? {16'b0 , RegMemo[15:0]} :
+                   (LoadControl == 3)? RegMemo :
+                   32'hxxxxxxxx;
 
 endmodule
-
-
--- Projeto IH/Store.v
+++ Projeto IH/Store.v
@@ -1,23 +1,9 @@
 module Store (
-    input wire [31:0] RegMemo, RegB, input wire [1:0] StoreControl, output reg [31:0] saida);
+    input wire [31:0] RegMemo, RegB, input wire [1:0] StoreControl, output wire [31:0] saida);
     
-    always @(posedge StoreControl) begin
-        if (StoreControl == 1) begin
-            saida = {RegMemo[31:8], RegB[7:0]}; 
-        end
-
-        else if (StoreControl == 2) begin
-            saida = {RegMemo[31:16], RegB[15:0]}; 
-        end
-
-        else if (StoreControl == 3) begin
-            saida = RegB; 
-        end
-
-        else begin
-            saida = 32'hxxxxxxxx; 
-        end
-        
-    end
+    assign  saida = (StoreControl == 1)? {RegMemo[31:8], RegB[7:0]} :
+                    (StoreControl == 2)? {RegMemo[31:16], RegB[15:0]} :
+                    (StoreControl == 3)? RegB :
+                    32'hxxxxxxxx;
 
 endmodule
